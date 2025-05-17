2347a57489408770be5a89f9734f2c6eaa57dbe6
Cary_C <1577045857@qq.com>
2025-05-17 23:40:56 +0800

    排序

--- J20250515-sort/src/Sort.java
+++ J20250515-sort/src/Sort.java
@@ -31,7 +31,7 @@ public class Sort {
      */
     public static void shellSort(int[] array) {
         int gap = array.length;
-        while (gap > 1) {
+        while (gap >= 1) {
             shell(array,gap);
             gap = gap/2;
         }
@@ -79,9 +79,17 @@ public class Sort {
 
     /**
      * 堆排序
+     *  时间复杂度O(N*logN)
+     *  空间复杂度O(1)
+     *  稳定性:稳定
      */
     public static void heapSort(int[] array) {
-        int end = array.length;
+        int end = array.length-1;
+
+        //创建大根堆
+        for (int parent = (end-1)/2; parent >= 0; parent-- ) {
+            siftDown(array,parent,array.length-1);
+        }
         while (end > 0){
             swap(array,0,end);
             siftDown(array,0,end);
@@ -89,9 +97,10 @@ public class Sort {
         }
     }
 
-    private static void siftDown(int[] array, int parent,int length) {
+    public static void siftDown(int[] array, int parent,int length) {
         int child = 2*parent + 1;
         while (child < length) {
+            //判断是否有右树
             if (child + 1 < length && array[child] < array[child+1]) {
                 child++;
             }
@@ -103,5 +112,37 @@ public class Sort {
                 break;
             }
         }
+    }
+
+    /**
+     * 冒泡排序
+     *  时间复杂度O(N^2)
+     *    最好情况是{1，2，3，4，5} O(N)
+     *  空间复杂度O(1)
+     *  稳定性:稳定
+     * @param array
+     */
+    public static void bubbleSort(int[] array) {
+        //i表示趟数
+        for (int i = 0; i < array.length-1; i++) {
+            boolean flg = false;//优化
+            for (int j = 0; j < array.length-1-i; j++) {
+                if (array[j] > array[j+1]) {
+                    swap(array,j,j+1);
+                    flg = true;
+                }
+            }
+            if (!flg){
+                break;
+            }
+        }
+    }
+
+    public static void quickSort(int[] array){
+        //接口统一性 ---重载
+        quickSort(array,0,array.length-1);
+    }
+    private static void quickSort(int[] array,int left,int right){
+        
     }
 }
--- J20250515-sort/src/Test.java
+++ J20250515-sort/src/Test.java
@@ -1,2 +1,127 @@
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Random;
+
 public class Test {
+    public class art{
+        public static void order(int[] array){
+            for (int i = 0; i < array.length; i++) {
+                array[i] = i;
+            }
+        }
+
+        public static void notOrder(int[] array) {
+            /*int j = 0;
+            for (int i = array.length-1; i >= 0 ; i--) {
+                array[j++] = i;
+            }*/
+            for (int i = 0; i < array.length; i++) {
+                array[i] = array.length-i;
+            }
+        }
+        public static void RandomOrder(int[] array) {
+            Random random = new Random();
+            for (int i = 0; i < array.length; i++) {
+                array[i] = random.nextInt(100);
+            }
+        }
+    }
+    public class SortTest{
+        public static void insertSortTest(int[] array) {
+            int[] arr = Arrays.copyOf(array,array.length);
+            long startTimes = System.currentTimeMillis();
+            Sort.insertSort(arr);
+            long endTimes = System.currentTimeMillis();
+            System.out.println("插入排序用时："+(endTimes - startTimes));
+        }
+        public static void shellSortTest(int[] array) {
+            int[] arr = Arrays.copyOf(array,array.length);
+            long startTimes = System.currentTimeMillis();
+            Sort.shellSort(arr);
+            long endTimes = System.currentTimeMillis();
+            System.out.println("希尔排序用时："+(endTimes - startTimes));
+        }
+        public static void heapSortTest(int[] array) {
+            int[] arr = Arrays.copyOf(array,array.length);
+            long startTimes = System.currentTimeMillis();
+            Sort.heapSort(arr);
+            long endTimes = System.currentTimeMillis();
+            System.out.println("堆排序用时："+(endTimes - startTimes));
+        }
+        public static void bubbleSortTest(int[] array) {
+            int[] arr = Arrays.copyOf(array,array.length);
+            long startTimes = System.currentTimeMillis();
+            Sort.bubbleSort(arr);
+            long endTimes = System.currentTimeMillis();
+            System.out.println("冒泡排序用时："+(endTimes - startTimes));
+        }
+    }
+
+
+    public static void main1(String[] args) {
+        int[] arr = new int[1_000];
+        art.RandomOrder(arr);
+        SortTest.bubbleSortTest(arr);
+        SortTest.heapSortTest(arr);
+        SortTest.shellSortTest(arr);
+        SortTest.insertSortTest(arr);
+    }
+    public static void main(String[] args) {
+        int[] arr = {10,5,3,73,25,26,142,46};
+        Sort.heapSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    class solution{
+        //挖孔法
+        public static int parttion(int[] array,int low,int high) {
+            int tmp = array[low];
+            while (high > low) {
+                while (array[high] >= tmp) {
+                    high--;
+                }
+                array[low] = array[high];
+
+                while (low < high) {
+                    if (array[low] <= tmp) {
+                        low++;
+                    }
+                    array[high] = array[low];
+                }
+            }
+
+            array[low] = tmp;
+            return low;
+        }
+
+        public static void quickSortNor(int[] array) {
+            int start = 0;
+            int end = array.length-1;
+            int par = parttion(array,start,end);
+
+            Deque<Integer> stack = new LinkedList<>();
+            if (par > start+1) {
+                stack.push(start);
+                stack.push(par-1);
+            }
+            if (par < end - 1) {
+                stack.push(par + 1);
+                stack.push(end);
+            }
+            while (!stack.isEmpty()){
+                end = stack.poll();
+                start = stack.poll();
+                par = parttion(array,start,end);
+                if (par > start+1) {
+                    stack.push(start);
+                    stack.push(par-1);
+                }
+                if (par < end - 1) {
+                    stack.push(par + 1);
+                    stack.push(end);
+                }
+            }
+        }
+    }
 }
