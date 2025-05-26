b8af2936efbad5c9780a5d80388a73397800570d
Cary_C <1577045857@qq.com>
2025-05-26 23:53:31 +0800

    练习

--- Practise/src/Test.java
+++ Practise/src/Test.java
@@ -3,11 +3,15 @@ import java.util.Random;
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println();
+    }
+    public static void main1(String[] args) {
         String str = "adbs";
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
         }
         Random random = new Random();
         random.nextInt();
+
     }
 }
