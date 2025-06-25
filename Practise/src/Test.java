f9f766abdaaecab4b236975059a77020b652d49d
Cary_C <1577045857@qq.com>
2025-06-25 20:09:39 +0800

    练习

--- Practise/src/Test.java
+++ Practise/src/Test.java
@@ -65,6 +65,6 @@ public class Test {
 
     public static void main(String[] args) {
         //System.out.println(containsNearbyDuplicate(nums, 2));
-        System.out.println("练习1");
+        System.out.println("练习12");
     }
 }
